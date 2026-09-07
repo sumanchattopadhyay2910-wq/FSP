b3fcf1c5c00281381845739297da11ae4d874175
unknown <sumanchatterjee2910@gmail.com>
2026-09-07 11:17:05 +0530

    fsp3.c

---
+++ fsp3.c
@@ -0,0 +1,28 @@
+// write a array program in c to find the sorted array in ascending order.
+#include <stdio.h>
+int main() {
+    int n, i, j, temp;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Sorting the array in ascending order
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("The sorted array in ascending order is: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
